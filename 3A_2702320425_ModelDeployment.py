8cc5b14ed0aa83dbce642dc6238c11939009dc21
nad-777a <nadja.nayaraaa@gmail.com>
2025-04-22 01:14:54 +0700

    3A_2702320425_ModelDeployment.py

--- 3A_2702320425_ModelDeployment.py
+++ 3A_2702320425_ModelDeployment.py
@@ -4,7 +4,7 @@ import numpy as np
 import joblib
 
 # Load the machine learning model and encoders
-model = joblib.load("bestxgb_model.pkl")
+model = joblib.load("best_xgbmodel.pkl")
 gender_encoder = joblib.load("person_gender_encoder.pkl")
 default_encoder = joblib.load("previous_loan_defaults_on_file_encoder.pkl")
 
